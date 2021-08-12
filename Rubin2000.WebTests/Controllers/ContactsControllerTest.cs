6d09fa464371f79592f389ecefd2def54b744d04
TedStanoev <64153082+TedStanoev@users.noreply.github.com>
2021-08-12 20:35:01 +0300

    Added testing for Contacts Controller

---
+++ Rubin2000.WebTests/Controllers/ContactsControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using MyTested.AspNetCore.Mvc;
+using Rubin2000.Web.Controllers;
+using Xunit;
+
+namespace Rubin2000.WebTests.Controllers
+{
+    public class ContactsControllerTest
+    {
+        [Fact]
+        public void IndexShouldReturnView()
+            => MyController<ContactsController>
+                .Instance()
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View();
+    }
+}
---
+++ Rubin2000.WebTests/Routing/ContactsControllerRouting.cs
@@ -0,0 +1,16 @@
+﻿using MyTested.AspNetCore.Mvc;
+using Rubin2000.Web.Controllers;
+using Xunit;
+
+namespace Rubin2000.WebTests.Routing
+{
+    public class ContactsControllerRouting
+    {
+        [Fact]
+        public void IndexShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/Contacts")
+                .To<ContactsController>(c => c.Index());
+    }
+}
