a220197d3dece87920aff3d6a10731c0dcacdd8f
TedStanoev <64153082+TedStanoev@users.noreply.github.com>
2021-08-14 21:26:40 +0300

    Added some tests for SchedulesController

--- Rubin2000.WebTests/Areas/Admin/Controllers/SchedulesControllerTest.cs
+++ Rubin2000.WebTests/Areas/Admin/Controllers/SchedulesControllerTest.cs
@@ -1,7 +1,51 @@
-﻿namespace Rubin2000.WebTests.Areas.Admin.Controllers
+﻿using MyTested.AspNetCore.Mvc;
+using Rubin2000.Services.ForEmployees.Models;
+using Rubin2000.Services.ForSchedules.Models;
+using Rubin2000.Web.Areas.Admin.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+using static Rubin2000.Global.WebConstants;
+using static Rubin2000.Global.WebTestConstants;
+using static Rubin2000.WebTests.Areas.Admin.Data.SchedulesControllerData;
+
+namespace Rubin2000.WebTests.Areas.Admin.Controllers
 {
     public class SchedulesControllerTest
     {
+        [Fact]
+        public void ControllerShouldHaveAuthorizeAttributeForAdminsOnly()
+            => MyController<SchedulesController>
+                .ShouldHave()
+                .Attributes(attributes => attributes
+                    .SpecifyingArea(AdminArea)
+                    .RestrictingForAuthorizedRequests(AdminRole));
+
+        [Fact]
+        public void IndexShouldReturnView()
+            => MyController<SchedulesController>
+                .Instance(i => i.WithUser(u => u.InRole(AdminRole))
+                    .AndAlso()
+                    .WithData(EmployeesWithSchedules()))
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<IEnumerable<EmployeeServiceModel>>(
+                        m => m.Count() == 2));
+
+        [Theory]
+        [InlineData(ScheduleId)]
+        public void EmployeeScheduleShouldReturnView(string id)
+            => MyController<SchedulesController>
+                .Instance(i => i.WithUser(u => u.InRole(AdminRole))
+                    .AndAlso()
+                    .WithData(Schedule()))
+                .Calling(c => c.EmployeeSchedule(id))
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<IEnumerable<EmployeeScheduleAppointmentServiceModel>>());
+
 
     }
 }
---
+++ Rubin2000.WebTests/Areas/Admin/Data/SchedulesControllerData.cs
@@ -0,0 +1,74 @@
+﻿using Rubin2000.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using static Rubin2000.Global.FormatConstants;
+using static Rubin2000.Global.WebTestConstants;
+
+namespace Rubin2000.WebTests.Areas.Admin.Data
+{
+    public static class SchedulesControllerData
+    {
+        public static Schedule Schedule()
+            => new()
+            {
+                Id = ScheduleId,
+                Employee = new Employee
+                {
+                    ScheduleId = ScheduleId
+                },
+                Appointments = Enumerable.Range(0, 2)
+                    .Select(a => new Appointment
+                    {
+                        ScheduleId = ScheduleId,
+                    })
+                    .ToList()
+            };
+
+        public static IEnumerable<Appointment> AppointmentsWithSchedule()
+            => Enumerable.Range(0, 2)
+                .Select(a => new Appointment
+                {
+                    ScheduleId = ScheduleId
+                });
+
+        public static IEnumerable<Employee> EmployeesWithSchedules()
+        {
+            var employeeList = new List<Employee>(2);
+
+            var employee1 = new Employee
+            {
+                Id = "EmployeeId1",
+                Name = "EmployeeName1",
+                Schedule = new Schedule
+                {
+                    Id = "Schedule1Id",
+                    StartsAt = DateTime.ParseExact(
+                        "03.02.2020 10:00", DateViewFormat + " " + TimeViewFormat, null),
+                    EndsAt = DateTime.ParseExact(
+                        "03.02.2020 18:00", DateViewFormat + " " + TimeViewFormat, null)
+                }
+            };
+
+            var employee2 = new Employee
+            {
+                Id = "EmployeeId2",
+                Name = "EmployeeName2",
+                Schedule = new Schedule
+                {
+                    Id = "Schedule2Id",
+                    StartsAt = DateTime.ParseExact(
+                        "03.02.2020 10:00", DateViewFormat + " " + TimeViewFormat, null),
+                    EndsAt = DateTime.ParseExact(
+                        "03.02.2020 18:00", DateViewFormat + " " + TimeViewFormat, null)
+                }
+            };
+
+            employeeList.Add(employee1);
+            employeeList.Add(employee2);
+
+            return employeeList;
+        }
+    }
+}
